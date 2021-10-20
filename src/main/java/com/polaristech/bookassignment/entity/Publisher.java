a177e9a9af048b914dd0aecbf7ceefea6b274595
Adeshina <91054814+integral1@users.noreply.github.com>
2021-10-20 22:56:06 +0100

    corrected the table issue

--- src/main/java/com/polaristech/bookassignment/entity/Publisher.java
+++ src/main/java/com/polaristech/bookassignment/entity/Publisher.java
@@ -13,18 +13,17 @@ import java.util.UUID;
 @Data
 @Builder
 @AllArgsConstructor
-@Entity(name = "publisher")
+@Entity
 @Table(name = "publisher")
 public class Publisher {
-    @NotNull
-    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
-    Date dateCreated;
-    @Column(columnDefinition = "TIMESTAMP WITH TIMEZONE DEFAULT NULL")
-    Date dateModified;
     @Id
     private UUID id;
     @Column
     private String publisherName;
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    Date dateCreated;
+    @Column(columnDefinition = "TIMESTAMP WITH TIME ZONE DEFAULT NULL")
+    Date dateModified;
     @NotNull
     private UUID createdBy;
     @OneToMany(mappedBy = "id", fetch = FetchType.LAZY, cascade = CascadeType.ALL )
